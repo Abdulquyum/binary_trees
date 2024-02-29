053ac7331cfa7a267ed31f67d68cdef96eda05cd
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-02-29 14:14:41 +0100

    add a node to the left of the tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,10 +18,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
-	{
-		newNode->left = parent->left;
+		return (NULL);
+
+	newNode->left = parent->left;
+
+	if (parent->left != NULL)
 		parent->left->parent = newNode;
-	}
 
 	parent->left = newNode;
 
