1455717c32a35747df75d0ae5c8ddcfc9c466b4c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-03-01 09:32:09 +0100

    measure the dept of a node of a tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * size_t binary_tree_depth - measures the depth of a node in a tree
+ * binary_tree_depth - measures the depth of a node in a tree
  *
  * @tree: pointer to the node to measure the depth
  *
@@ -14,5 +14,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (1 + binary_tree_depth(tree->parent));
 	}
 
-	return (0)
+	return (0);
 }
