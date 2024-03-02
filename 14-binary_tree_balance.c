ac2600315354ac5c569f3df5761cc45a9eac9c24
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-03-02 01:13:33 +0100

    counts the nodes with at least 1 child in a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,7 +8,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
+	if (tree != NULL)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
 	return (0);
@@ -22,7 +22,7 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
+	if (tree != NULL)
 	{
 		size_t l = 0, r = 0;
 
